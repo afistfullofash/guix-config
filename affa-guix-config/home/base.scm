b96b41088cabc668fa4fc9b3948c5d70316260ef
Natalie Atkinson <natalie.atkinson95@pm.me>
2025-11-20 08:26:14 +1100

    fix: guix: Use codeburg mirror for guix

--- affa-guix-config/home/base.scm
+++ affa-guix-config/home/base.scm
@@ -281,6 +281,15 @@
 		  home-channels-service-type
 		  (list
 		   (channel
+		     (name 'guix)
+		     (url "https://codeberg.org/guix/guix.git")
+		     (branch "master")
+		     (introduction
+		      (make-channel-introduction
+		       "9edb3f66fd807b096b48283debdcddccfea34bad"
+		       (openpgp-fingerprint
+			"BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+		   (channel
 		    (name 'afistfullofash)
 		    (url "https://github.com/afistfullofash/afistfullofash")
 		    (branch "main"))
